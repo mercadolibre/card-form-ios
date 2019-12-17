55735daba812287296ddb09ba58414bc944cdf69
Juan Sanzone <juan.sanzone@gmail.com>
2019-12-17 10:27:17 -0300

    Add docu updated and script

---
+++ updateDocu.sh
@@ -0,0 +1,9 @@
+jazzy --podspec MLCardForm.podspec \
+  --clean \
+  --author Mercadopago \
+  --author_url https://github.com/mercadolibre/card-form-ios \
+  --github_url https://github.com/mercadolibre/card-form-ios \
+  --github-file-prefix https://github.com/mercadolibre/card-form-ios/tree/develop \
+  --module-version 1.0.0 \
+  --root-url https://mercadolibre.github.io/card-form-ios \
+  --output docs \
