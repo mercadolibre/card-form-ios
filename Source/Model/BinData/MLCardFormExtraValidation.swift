f5de8444bf0d8db241296d9ae89710858ecd56d1
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-04-30 09:12:41 -0300

    Add dtos

---
+++ Source/Model/BinData/MLCardFormExtraValidation.swift
@@ -0,0 +1,15 @@
+//
+//  MLCardFormExtraValidation.swift
+//  MLCardForm
+//
+//  Created by Esteban Adrian Boffa on 28/04/2020.
+//
+
+import Foundation
+
+struct MLCardFormExtraValidation: Codable {
+
+    let name: String?
+    let value: String?
+    let errorMessage: String?
+}
