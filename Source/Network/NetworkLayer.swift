eaf261fa15c92638ecb62c4010eac53923c69d03
Eric Ertl <eric.ertl@mercadolibre.com>
2020-12-16 13:22:58 -0300

    Network info for debug

--- Source/Network/NetworkLayer.swift
+++ Source/Network/NetworkLayer.swift
@@ -74,6 +74,10 @@ struct NetworkLayer {
         }
 
         let session = URLSession(configuration: .default)
+        
+        #if DEBUG
+        print("\nHTTP request: \(urlRequest.url?.absoluteString ?? "")\nParams: \(router.body?.prettyPrintedJSONString ?? "")\n")
+        #endif
 
         let dataTask = session.dataTask(with: urlRequest) { data, response, error in
             guard error == nil else {
@@ -88,6 +92,9 @@ struct NetworkLayer {
                 completion(.failure(NetworkLayerError.response))
                 return
             }
+            #if DEBUG
+            print("\nHTTP response: \(response.url?.absoluteString ?? "")\nParams: \(data.prettyPrintedJSONString ?? "")\n")
+            #endif
             guard (200...299).contains(response.statusCode) else {
                 var message = ""
                 do {
@@ -137,5 +144,15 @@ struct NetworkLayer {
                 }
             }).resume()
         }
+    }
+}
+
+extension Data {
+    var prettyPrintedJSONString: NSString? { /// NSString gives us a nice sanitized debugDescription
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
     }
 }
